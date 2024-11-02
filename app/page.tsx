2ee1990289b579ec3cc7f258ddb8608d2b274274
Sergey87 <serge.techno@gmail.com>
2024-11-02 17:11:46 +0300

    work with filters - 1 part

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
-import { Container, Title } from '@/components/shared';
-import { Categories } from '@/components/shared/categories';
+import { Container, SortPopup, Title } from '@/components/shared';
+import { Filters } from '@/components/shared/filters';
+import { TopBar } from '@/components/shared/top-bar';
 
 export default function Home() {
   return (
     <>
       <Container className='mt-10'>
         <Title text='Все пиццы' size='lg' />
+      </Container>
+      <TopBar />
+      {/* <div style={{ height: '3000px' }}></div> */}
 
-        <Categories />
+      <Container className='pb-14'>
+        {/* фильтрация */}
+        <div className='flex gap-[60px]'>
+          <Filters />
+        </div>
+
+        {/* список товаров */}
+        <div className='flex-1'>
+          <div className='flex flex-col gap-16'>Список товаров</div>
+        </div>
       </Container>
     </>
   );
